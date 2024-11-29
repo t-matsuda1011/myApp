01e639bb5abc00799650a8740e8015bd235e3957
t-matsuda1 <t-matsuda@netyear.net>
2024-11-29 16:56:34 +0900

    Imageコンポーネントの修正

--- src/components/under-constraction/index.tsx
+++ src/components/under-constraction/index.tsx
@@ -5,7 +5,7 @@ const UnderConstruction = () => {
     return (
         <div className="mt-10 mx-auto p-10 bg-white md:w-4/5 w-11/12 rounded-xl shadow-md text-center">
             <p className='md:pt-10 pt-5'>ページ作成中…</p>
-            <img src="/underConstruction.png" alt="工事中" className='mx-auto md:w-5/12 w-3/6'/>
+            <Image src="/underConstruction.png" alt="工事中" className='mx-auto md:w-5/12 w-3/6' width={500} height={500}/>
         </div>
     );
 };

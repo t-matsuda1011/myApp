aaf72ecebbf60420d4a94b9d870e0285017011bd
t-matsuda1 <t-matsuda@netyear.net>
2024-11-29 16:34:53 +0900

    imgタグに変更

--- src/components/under-constraction/index.tsx
+++ src/components/under-constraction/index.tsx
@@ -5,7 +5,7 @@ const UnderConstruction = () => {
     return (
         <div className="mt-10 mx-auto p-10 bg-white md:w-4/5 w-11/12 rounded-xl shadow-md text-center">
             <p className='md:pt-10 pt-5'>ページ作成中…</p>
-            <Image src="/underConstruction.png" alt="工事中" className='mx-auto md:w-5/12 w-3/6'/>
+            <img src="/underConstruction.png" alt="工事中" className='mx-auto md:w-5/12 w-3/6'/>
         </div>
     );
 };
